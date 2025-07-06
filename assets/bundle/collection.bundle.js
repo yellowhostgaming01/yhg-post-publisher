81c6974eefd0a5ae318331d5f81c496cc1a011d3
yellowhostgaming01 <145857693+yellowhostgaming01@users.noreply.github.com>
2025-07-06 15:16:19 +0530

    Add files via upload

--- assets/bundle/collection.bundle.js
+++ assets/bundle/collection.bundle.js
@@ -157,16 +157,46 @@ const genericData = {
 };
 
 const socialMedias = {
-  insta: [ "https://instagram.com/yellowhostgaming", "https://www.instagram.com/j_k_art_?igsh=OHp0ajFlcXljdXFk", "https://www.instagram.com/king._.nikhil009?igsh=Z3B1eWJucDdtdjRp"],
-  yt: ["https://youtube.com/@yellowhostgaming","https://youtube.com/@khodiyarart88", "https://youtube.com/@nikhil0098"],
+  insta: [
+    "https://instagram.com/yellowhostgaming",
+    "https://www.instagram.com/j_k_art_?igsh=OHp0ajFlcXljdXFk",
+    "https://www.instagram.com/king._.nikhil009?igsh=Z3B1eWJucDdtdjRp",
+  ],
+  yt: [
+    "https://youtube.com/@yellowhostgaming",
+    "https://youtube.com/@khodiyarart88",
+    "https://youtube.com/@nikhil0098",
+  ],
   gmail: [
     "yellowhostgaming1@gmail.com",
     "rohanpatel267774@gmail.com",
-    "nikhilmadhavani09@gmail.com"
+    "nikhilmadhavani09@gmail.com",
   ],
   whatsappGroup: [
     "https://chat.whatsapp.com/LbOJrGDWGuqLtygEZQU9uj",
     "https://chat.whatsapp.com/IwlLsyVFH4ILo1BAcLCZMr",
-    "#"
-  ]
+    "#",
+  ],
+};
+
+const modType = {
+  assetBased: {
+    instructions:
+      `    <p>1️⃣ Extract the zip file to a folder.</p>\n` +
+      `    <p>2️⃣ Select the files you wish to add.</p>\n` +
+      `    <p>3️⃣ Copy or Cut the selected files.</p>\n` +
+      `    <p>4️⃣ Navigate to <strong>Android/obb/com.maleo.bussimulatorid/assets/bin/data</strong></p>\n` +
+      `    <p>5️⃣ Paste the files into the designated folder.</p>\n` +
+      `    <p>✅ Open BUSSID and enjoy your new mod!</p>\n`,
+  },
+  livery: {
+    instructions:
+      `    <p>1️⃣ If the file is a <strong>.zip</strong>, first extract it to access the .png file.</p>\n` +
+      `    <p>2️⃣ Launch the <strong>Bus Simulator Indonesia</strong> (BUSSID) game.</p>\n` +
+      `    <p>3️⃣ Go to the <strong>Garage</strong> section and choose your vehicle.</p>\n` +
+      `    <p>4️⃣ Tap on the <strong>"Livery"</strong> option.</p>\n` +
+      `    <p>5️⃣ Select <strong>"Select File"</strong> and browse to the extracted folder or directly to the downloaded .png file.</p>\n` +
+      `    <p>6️⃣ Choose the livery image and apply it to your vehicle.</p>\n` +
+      `    <p>✅ Done! Your livery has been applied successfully.</p>\n`,
+  }
 };
